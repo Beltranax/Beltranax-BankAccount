b66c157e338d76cc638e9a84ca09b7402805e1c7
Beltranax <beltranangel7293@gmail.com>
2023-02-02 02:49:17 +0000

    Lab Completed

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,8 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance = balance + amount;
+        
     }
 
     /**
@@ -32,7 +33,12 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(balance < amount ){
+        return;
+        } else {
+            balance = balance - amount;
+        }
+        
     }
 
     /**
@@ -40,6 +46,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
